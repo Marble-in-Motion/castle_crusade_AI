5f898eaa9c8659c7c448c673dd043e04088cbd40
Spencer Warren <spewarren@gmail.com>
2018-05-05 17:31:56 +0100

    make thread safe

--- scripts/neural_socket.py
+++ scripts/neural_socket.py
@@ -26,7 +26,9 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import send_from_directory
+from flask_restful import Resource, Api
 app = Flask(__name__)
+api = Api(app)
 
 model_file = r"C:\Users\SP\Documents\WORK\GP\tensorflow\tensorflow-for-poets-2\tf_files\retrained_graph.pb"
 label_file = r"C:\Users\SP\Documents\WORK\GP\tensorflow\tensorflow-for-poets-2\tf_files\retrained_labels.txt"
@@ -36,64 +38,68 @@ scores_team1 = [0, 0, 0, 0, 0]
 scores_team2 = [0, 0, 0, 0, 0]
 
 
-@app.route("/inference/<int:team>", methods=['GET'])
-def inference(team=1):
-    def read_tensor_from_image_file(file_path, input_height=224, input_width=224, input_mean=128, input_std=128):
-        file_reader = tf.read_file(file_path, "file_reader")
-        if file_path.endswith(".png"):
-            image_reader = tf.image.decode_png(file_reader, channels=3, name='png_reader')
-        elif file_path.endswith(".gif"):
-            image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name='gif_reader'))
-        elif file_path.endswith(".bmp"):
-            image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
-        else:
-            image_reader = tf.image.decode_jpeg(file_reader, channels=3, name='jpeg_reader')
+class Inference(Resource):
+    def get(self, team=1):
+        def read_tensor_from_image_file(file_path, input_height=224, input_width=224, input_mean=128, input_std=128):
+            file_reader = tf.read_file(file_path, "file_reader")
+            if file_path.endswith(".png"):
+                image_reader = tf.image.decode_png(file_reader, channels=3, name='png_reader')
+            elif file_path.endswith(".gif"):
+                image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name='gif_reader'))
+            elif file_path.endswith(".bmp"):
+                image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+            else:
+                image_reader = tf.image.decode_jpeg(file_reader, channels=3, name='jpeg_reader')
 
-        float_caster = tf.cast(image_reader, tf.float32)
-        dims_expander = tf.expand_dims(float_caster, 0)
-        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-        return tf.Session().run(normalized)
+            float_caster = tf.cast(image_reader, tf.float32)
+            dims_expander = tf.expand_dims(float_caster, 0)
+            resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+            normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+            return tf.Session().run(normalized)
 
-    def load_labels(label_file):
-        label = []
-        proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-        for l in proto_as_ascii_lines:
-            label.append(l.rstrip())
-        return label
+        def load_labels(label_file):
+            label = []
+            proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+            for l in proto_as_ascii_lines:
+                label.append(l.rstrip())
+            return label
 
-    print("inference called, team: " + str(team))
+        print("inference called, team: " + str(team))
 
-    output_values = []
-    for file_path in glob.glob(screenshot_dir + '\Team' + str(team) + "\*.jpg"):
-        t = read_tensor_from_image_file(file_path)
+        output_values = []
+        for file_path in glob.glob(screenshot_dir + '\Team' + str(team) + "\*.jpg"):
+            t = read_tensor_from_image_file(file_path)
 
-        results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
-        results = np.squeeze(results)
+            results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
+            results = np.squeeze(results)
 
-        top_k = results.argsort()[-5:][::-1]
-        labels = load_labels(label_file)
+            top_k = results.argsort()[-5:][::-1]
+            labels = load_labels(label_file)
 
-        total = 0
-        for i in top_k:
-            total += int(labels[i]) * results[i]
+            total = 0
+            for i in top_k:
+                total += int(labels[i]) * results[i]
 
-        output_values.append(format(total, '.3f'))
+            output_values.append(format(total, '.3f'))
 
-    if team == 1:
-        global scores_team1
-        scores_team1 = output_values
-    elif team == 2:
-        global scores_team2
-        scores_team2 = output_values
+        if team == 1:
+            global scores_team1
+            scores_team1 = output_values
+        elif team == 2:
+            global scores_team2
+            scores_team2 = output_values
 
-    return ','.join(map(str, output_values))
+        return ','.join(map(str, output_values))
 
 
-@app.route('/image/<int:team>/<path:filename>')
-def image(team, filename):
-    path = screenshot_dir + '\Team' + str(team)
-    return send_from_directory(path, filename)
+class Image(Resource):
+    def get(self, team, filename):
+        path = screenshot_dir + '\Team' + str(team)
+        return send_from_directory(path, filename)
+
+
+api.add_resource(Inference, '/inference/<int:team>')
+api.add_resource(Image, '/image/<int:team>/<path:filename>')
 
 
 @app.route("/<int:team>", methods=['GET'])
@@ -138,5 +144,5 @@ if __name__ == "__main__":
     print("done")
 
     # set-up website
-    app.run(debug=True, use_reloader=False)
+    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False, threaded=True)
 
